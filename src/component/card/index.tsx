d39ddb4c4ea079450a70a43a8c3a2fe533768b26
yomua <yomua.yhw@gmail.com>
2022-03-19 16:12:18 +0000

    add Card

---
+++ src/component/card/index.tsx
@@ -0,0 +1,89 @@
+import React, { useState } from 'react'
+import { Tag, Card, Image, CardProps } from 'antd'
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock, faUserAstronaut } from '@fortawesome/free-solid-svg-icons'
+import style from './index.less'
+
+interface CustomProps extends CardProps {
+    img?: string
+    text?: string
+    title?: string
+    author?: string
+    time?: string
+    tag?: { icon: IconProp; name: string; color?: string }[]
+}
+
+const CustomCard = (props: CustomProps) => {
+    const {
+        img,
+        time,
+        text,
+        title,
+        author,
+        className,
+        tag = [],
+        ...otherProps
+    } = props
+
+    const [visible, setVisible] = useState(false)
+
+    return (
+        <Card
+            bordered
+            hoverable
+            className={`${style.card} ${className}`}
+            {...otherProps}
+        >
+            <Image
+                src={img}
+                className={style.image}
+                preview={{ visible: false }}
+                onClick={() => setVisible(true)}
+            />
+
+            <div className={style.preview}>
+                <Image.PreviewGroup
+                    preview={{
+                        visible,
+                        maskClosable: false,
+                        onVisibleChange: (vis) => setVisible(vis),
+                    }}
+                >
+                    <Image src={img} />
+                </Image.PreviewGroup>
+            </div>
+
+            <div className={style.content}>
+                <div className={style.title}>{title}</div>
+
+                <div className={style.summary}>{text}</div>
+
+                <div className={style.info}>
+                    <div>
+                        <FontAwesomeIcon icon={faUserAstronaut} /> {author}
+                    </div>
+                    <div>
+                        <FontAwesomeIcon icon={faClock} /> {time}
+                    </div>
+                </div>
+
+                <div className={style.tags}>
+                    {tag.map((tag) => {
+                        return (
+                            <Tag
+                                icon={<FontAwesomeIcon icon={tag?.icon} />}
+                                color={tag?.color ?? '#55acee'}
+                                className={style.tag}
+                            >
+                                {tag?.name}
+                            </Tag>
+                        )
+                    })}
+                </div>
+            </div>
+        </Card>
+    )
+}
+
+export default CustomCard
--- src/component/index.tsx
+++ src/component/index.tsx
@@ -1,4 +1,5 @@
-export { default as Navigation } from './navigation';
-export { default as Direction } from './direction';
-export { default as TopBar } from './topbar';
-export { default as Text } from './text';
+export { default as Navigation } from './navigation'
+export { default as Direction } from './direction'
+export { default as TopBar } from './topbar'
+export { default as Text } from './text'
+export { default as Card } from './card'
