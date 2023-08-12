3ed12ba0a5b963ecff2facc48cd620d89254809b
yomua <yomua.yhw@gmail.com>
2023-08-12 13:53:32 +0000

    bugfix refresh article

--- src/pages/feature/index.tsx
+++ src/pages/feature/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-children-prop */
 import { ReactNode, memo } from 'react'
-import { useParams } from 'umi'
+import { useLocation, useParams } from 'react-router'
+import { Location } from 'history'
 
 import Error404 from '@/pages/404'
 
@@ -11,7 +12,21 @@ import Gpt3 from './gpt3'
 import Article from './article'
 import style from './index.less'
 
-function renderFeature(name: FeatureName): ReactNode {
+function renderFeature(
+    name: FeatureName,
+    options: {
+        location: Location
+    },
+): ReactNode {
+    const {
+        location: { pathname = '' },
+    } = options ?? {}
+
+    // 说明当前路由是 feature/article 的标题，就重定向回去
+    if (pathname.includes('heading')) {
+        return <Article />
+    }
+
     switch (name) {
         case FeatureName.Article:
             return <Article />
@@ -25,7 +40,6 @@ function renderFeature(name: FeatureName): ReactNode {
                     <Todo />
                 </div>
             )
-
         default:
             return <Error404 />
     }
@@ -33,8 +47,11 @@ function renderFeature(name: FeatureName): ReactNode {
 
 function Feature() {
     const { name } = useParams() as { name: FeatureName }
+    const location = useLocation()
 
-    return <div className={style.section}>{renderFeature(name)}</div>
+    return (
+        <div className={style.section}>{renderFeature(name, { location })}</div>
+    )
 }
 
 export default memo(Feature)
