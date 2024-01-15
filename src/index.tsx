102c665712c77bd819f6a04273a7b5c1e309bf54
yomua <yomua.yhw@gmail.com>
2024-01-15 18:02:47 +0800

    add: @yomua/y-indexeddb

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import {
     faYoutubeSquare,
     faFacebookSquare,
 } from '@fortawesome/free-brands-svg-icons'
-import IndexedDB from '~/packages/y-indexeddb'
+import IndexedDB from '@yomua/y-indexeddb'
 
 import style from './index.less'
 import Footer from './layouts/footer'
